70e1c89db642a3e69649fa93dc2ab49b3602c6a4
Shibbir Ahmed <shibbirweb@gmail.com>
2020-11-21 10:36:36 +0600

    Complete customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-blue': '#1992d4'
+      },
+      spacing: {
+        88: '120rem'
+      }
+    },
   },
   variants: {
     backgroundColor: ['responsive', 'hover', 'focus', 'active'],
